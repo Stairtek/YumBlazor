e5459b34385cac7d3488447c3d8d1a0c3c91c864
Mike Campbell <mike.campbell12@gmail.com>
2025-01-20 13:57:09 -0500

    Category CRUD - Part 87

---
+++ Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+using YumBlazor.Data;
+
+namespace YumBlazor.Repository.IRepository;
+
+public interface ICategoryRepository
+{
+    public Category Create(Category category);
+    
+    public Category Update(Category category);
+    
+    public bool Delete(int id);
+    
+    public Category Get(int id);
+    
+    public IEnumerable<Category> GetAll();
+}
