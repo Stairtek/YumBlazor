38aebffa9e801c983d5d3c849414776403db3114
Mike Campbell <mike.campbell12@gmail.com>
2025-01-21 10:57:41 -0500

    Authentication in Blazor - Part 118

---
+++ Utilities/StaticDetails.cs
@@ -0,0 +1,7 @@
+namespace YumBlazor.Utilities;
+
+public static class StaticDetails
+{
+    public static readonly string RoleAdmin = "Admin";
+    public static readonly string RoleCustomer = "Customer";
+}
